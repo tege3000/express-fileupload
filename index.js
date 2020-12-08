6a763a926375e7e57334dadb7d4b04d7464911ff
Tito <teetoegeonu@gmail.com>
2020-12-08 13:47:04 +0100

    ADD - Created simple file upload feature using express

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const upload = require('express-fileupload')
 const app = express()
 
 app.use(upload())
+app.use(express.static("public"));
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -11,8 +13,19 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     if(req.files) {
-        console.log(req.files)
+        const file = req.files.file
+        const filename = file.name
+
+        file.mv(`./uploads/${filename}`, function(err) {
+            if(err) {
+                res.send(err)
+            }
+            else {
+                res.send("File Uploaded")
+            }
+        })
     }
 })
 
 app.listen(3000)
+console.log("Server started on port 3000")
